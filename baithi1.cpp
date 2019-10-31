dd0b483c76b05b7149733b62c4c6fbad97aa01d0
gaconbietvo <gaconbietvo@gmail.com>
2019-10-31 20:05:39 +0700

    baithi

--- baithi1.cpp
+++ baithi1.cpp
@@ -1,15 +1,15 @@
 #include <stdio.h>
 int main(){
-	int f0 = 0;
+	int f0 = 1;
 	int f1 = 1;
-	int f2 = 1;
+	int f2 = 2;
 	printf("Fibonacci series between 1 and 100:\n");
-	for (int i = 0; f2 < 89; i++)
+	for (int i = 0; f2 < 100; i++)
 	{
+		printf("%d  ",f2);
 		f0 = f1;
 		f1 = f2;
 		f2 = f0 + f1;
-		printf("%d  ",f2);
 	}
 	return 0;
 }
